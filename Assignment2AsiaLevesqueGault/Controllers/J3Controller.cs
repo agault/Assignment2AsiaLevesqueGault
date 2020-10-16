ba03a0b02d097597a61fdda524910f6fcafd1a36
17193 <asialevesquegault@gmail.com>
2020-10-15 19:56:01 -0600

    Added J3 thought process

---
+++ Assignment2AsiaLevesqueGault/Controllers/J3Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+/// <summary>
+/// This one is a doozy but I can see how it could possibly be done
+/// button 1-nothing 
+/// button 2-abc the imput for the method would be the number of pushes. 
+///         for loop for button 2 with if statements being the number of pushes
+///         save the input into an array to output words.
+/// It would be the same for the other button.
+
+/// </summary>
+namespace Assignment2AsiaLevesqueGault.Controllers
+{
+    public class J3Controller : ApiController
+    {
+    }
+}
